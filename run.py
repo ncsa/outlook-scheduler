0260531ff168fa33617e8cf63e62367f2105f764
Andy Loftus <aloftus@gmail.com>
2022-07-28 13:58:04 +0000

    create triage event

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 #!/bin/env python3
 
+# Configure logging
 import argparse
 import csv
 import datetime
@@ -10,54 +11,69 @@ import os
 import pprint
 import pyexch.pyexch
 
-def process_args():
-    constructor_args = {
-        'formatter_class': argparse.RawDescriptionHelpFormatter,
-        'description': 'triage duty scheduler',
-        'epilog': '''
-Program is controlled using the following environment variables:
-    NETRC
-        path to netrc file (default: ~/.netrc)
-        where netrc file has a key named "EXCH"
-        and the "EXCH" key has values for:
-        "login" in format username@illinois.edu
-        "password",
-        "account" in format username@illinois.edu
-        NOTE that account can be different than login,
-        which is how to access a shared calendar.
-'''
-    }
-    parser = argparse.ArgumentParser()
-    parser.add_argument( '-d', '--debug', action='store_true' )
-    parser.add_argument( '-f', '--infile', type=argparse.FileType() )
-    # parser.add_argument( '-k', '--netrckey',
-    #     help='key in netrc to use for login,passwd; default=%(default)s' )
-    defaults = {
-        'debug': False,
-        'infile': '-',
-    }
-    parser.set_defaults( **defaults )
-    args = parser.parse_args()
-    # # Load login and passwd from netrc
-    # netrc_fn = os.getenv( 'NETRC' )
-    # nrc = netrc.netrc( netrc_fn )
-    # nrc_parts = nrc.authenticators( args.netrckey )
-    # if nrc_parts:
-    #     args.user = nrc_parts[0]
-    #     args.passwd = nrc_parts[2]
-    # if not args.user:
-    #     raise UserWarning( 'Empty username not allowed' )
-    # if not args.passwd:
-    #     raise UserWarning( 'Empty passwd not allowed' )
-    return args
+# Hash to hold module level data
+resources = {
+    triage_location = 'https://illinois.zoom.us/j/87390897249?pwd=JZ8_SzMfvWmFMJp2hSizNx2vxQm4ZC.1'
+    triage_categories = [ 'TicketMaster' ],
+}
+
+def get_args():
+    if 'args' not in resources:
+        constructor_args = {
+            'formatter_class': argparse.RawDescriptionHelpFormatter,
+            'description': 'triage duty scheduler',
+            'epilog': '''
+    Program is controlled using the following environment variables:
+        NETRC
+            path to netrc file (default: ~/.netrc)
+            where netrc file has a key named "EXCH"
+            and the "EXCH" key has values for:
+            "login" in format username@illinois.edu
+            "password",
+            "account" in format username@illinois.edu
+            NOTE that account can be different than login,
+            which is how to access a shared calendar.
+    '''
+        }
+        parser = argparse.ArgumentParser()
+        parser.add_argument( '-d', '--debug', action='store_true' )
+        parser.add_argument( '-v', '--verbose', action='store_true' )
+        parser.add_argument( '-f', '--infile', type=argparse.FileType() )
+        # parser.add_argument( '-k', '--netrckey',
+        #     help='key in netrc to use for login,passwd; default=%(default)s' )
+        defaults = {
+            'debug': False,
+            'infile': '-',
+        }
+        parser.set_defaults( **defaults )
+        args = parser.parse_args()
+        resources['args'] = args
+	if args.verbose:
+            logger.setLevel( logging.INFO )
+        if args.debug:
+            logger.setLevel( logging.DEBUG )
+    return resources['args']
+
+def get_regex_map():
+    return {
+        "TRIAGE":"^Triage: ",
+        "SHIFTCHANGE":"^Triage Shift Change: ",
+        }
+
+def get_pyexch():
+    if 'pyexch' not in resources:
+        regex_map = get_regex_map()
+        resources['pyexch'] = pyexch.pyexch.PyExch( regex_map = regex_map )
+    return resources['pyexch']
 
 
-def get_existing_events( px, start, end ):
+def get_existing_events( start, end ):
     ''' px = PyExch instance, already logged in
         start = datetime.date OR datetime.datetime
         end = datetime.date OR datetime.datetime
         Get existing events between "start" and "end"
     '''
+    px = get_pyexch()
     # convert end to a datetime at the end of the day 11:59:59 PM
     existing_events = px.get_events_filtered(
         start = start,
@@ -75,6 +91,14 @@ def get_existing_events( px, start, end ):
     return current_events
 
 
+def get_triage_location():
+    return resources['triage_location']
+
+
+def get_triage_categories():
+    return resources['triage_categories']
+
+
 def next_business_day( date ):
     ''' Return the next weekday
     '''
@@ -82,38 +106,32 @@ def next_business_day( date ):
     return date + [1, 1, 1, 1, 3, 2, 1][date.weekday()]
 
 
-def new_triage_event( px, date, attendees, location ):
-    names = attendees.keys()
-    emails = attendees.values()
-    px.new_all_day_event(
-        date = date, 
-        subject = f"Triage: {','.join(names)}",
-        attendees = emails,
-        location = location,
-        categories = [ 'TicketMaster' ],
-        free = True,
-    )
-    # self.make_or_update_shift_change_event( date=date, attendees=attendees, location=location )
-    # next_day = self.next_business_day( date=date )
-    # self.make_or_update_shift_change_event( date=next_date, attendees=attendees, location=location )
-
-
-# def make_or_update_shift_change_event( self, date, attendees, location ):
-#     emails = attendees.values()
-#     existing_event = self.get_shift_change_mtg( date )
-#     if existing_event:
-
-
-def create_or_update_TRIAGE_event():
-    print( 'create_or_update_TRIAGE_event' )
+def create_or_update_TRIAGE_event( date, attendees, existing_event=None):
+    ''' date = datetime.date for new event
+        attendees = list of email addresses
+        existing_event = raw exchange event
+    '''
+    if existing_event:
+        logging.info( f'Ignoring existing event {pprint.pprint(ev)}' )
+    else:
+        px = get_pyexch()
+        px.new_all_day_event( 
+            date = date, 
+            subject = f"Triage: {','.join(names)}",
+            attendees = attendees,
+            location = get_triage_location(),
+            categories = get_triage_categories(),
+            free = True
+        )
 
 
 def create_or_update_SHIFTCHANGE_event():
     print( 'create_or_update_SHIFTCHANGE_event')
 
 
-def run( args ):
-    pprint.pprint( ['ARGS', args] )
+def run():
+    args= get_args()
+    # pprint.pprint( ['ARGS', args] )
 
     # get CSV input
     csv_data = csv.reader( args.infile, dialect='excel-tab' )
@@ -121,32 +139,27 @@ def run( args ):
     # pprint.pprint( triage_raw_data )
 
     # get existing events as dict[date][type]
-    regex_map = {
-        "TRIAGE":"^Triage: ",
-        "SHIFTCHANGE":"^Triage Shift Change: ",
-        }
-    px = pyexch.pyexch.PyExch( regex_map = regex_map )
     existing_events = get_existing_events(
-        px,
         start = min( triage_raw_data.keys() ),
         end = max( triage_raw_data.keys() ),
     )
 
-    for dt in sorted( existing_events.keys() ):
-        for typ, ev in current_events[dt].items():
-            subj = ev.subject
-            members = [ x.mailbox.email_address for x in ev.raw_event.required_attendees ]
-            pprint.pprint( [ dt, typ, subj, members ] )
+    # for dt in sorted( existing_events.keys() ):
+    #     for typ, ev in current_events[dt].items():
+    #         subj = ev.subject
+    #         members = [ x.mailbox.email_address for x in ev.raw_event.required_attendees ]
+    #         pprint.pprint( [ dt, typ, subj, members ] )
 
     # attempt to create triage meetings
     for dt, members in triage_raw_data.items():
-        for typ in regex_map.keys():
+        re_map = get_regex_map()
+        for typ in re_map.keys():
             try:
-                existing_event = existing_events[dt][typ]
+                ev = existing_events[dt][typ]
             except KeyError:
-                existing_event = None
+                ev = None
             fname = f'create_or_update_{typ}_event'
-            globals()[fname]()
+            globals()[fname]( date=dt, attendees=members, existing_event=ev)
 
     
 
@@ -165,7 +178,6 @@ def run( args ):
     # pyexch write test
     date = datetime.date( 2022, 6, 27 )
     attendees = { 'Loftus': 'aloftus@illinois.edu' }
-    location='https://illinois.zoom.us/j/87390897249?pwd=JZ8_SzMfvWmFMJp2hSizNx2vxQm4ZC.1'
     new_triage_event( px=px, date=date, attendees=attendees, location=location )
     # SAMPLE EVENT
     # SimpleEvent(
@@ -181,11 +193,10 @@ def run( args ):
 
 if __name__ == '__main__':
     log_lvl = logging.INFO
-    args = process_args()
+    args = get_args()
+    fmt = '%(levelname)s %(message)s'
     if args.debug:
         log_lvl = logging.DEBUG
-    fmt = '%(levelname)s %(message)s'
-    if log_lvl == logging.DEBUG:
         fmt = '%(levelname)s [%(filename)s:%(funcName)s:%(lineno)s] %(message)s'
     logging.basicConfig( level=log_lvl, format=fmt )
     no_debug = [
@@ -193,5 +204,5 @@ if __name__ == '__main__':
     ]
     for key in no_debug:
         logging.getLogger(key).setLevel(logging.CRITICAL)
-    run( args )
+    run()
 
